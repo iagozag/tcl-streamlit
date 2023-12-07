e6101b394df6fe8d63944df45150eb02037583a9
Iago Zagnoli Albergaria <iaguimzagnoli25@gmail.com>
2023-12-07 16:31:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+%%writefile app.py
+
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -87,9 +89,10 @@ def main():
 
             if params['b'] < params['a']:
                 st.error('The value of b must be greater than or equal to a.')
+                return;
 
-        params['qnt'] = st.number_input('Enter the number of samples (qnt >= 1):', min_value=1, value=100)
-        params['len'] = st.number_input('Enter the size of the samples (sz >= 1):', min_value=1, value=30)
+        params['qnt'] = st.number_input('Enter the number of samples (n >= 1):', min_value=1, value=100)
+        params['len'] = st.number_input('Enter the size of the samples (n >= 1):', min_value=1, value=30)
 
     if block_op == 'Binomial':
         st.markdown("""
